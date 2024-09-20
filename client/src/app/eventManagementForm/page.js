c03e51b3a4e659d81065d260e9bbb37932b72b95
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-09-19 19:31:38 -0500

    added design for landing page

---
+++ client/src/app/eventManagementForm/page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function EventManagementForm() {
+  return (
+    <section id="userProfile" className="w-screen h-screen" >
+     
+    </section>
+  );
+}
---
+++ client/src/app/userProfileManagement/page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function UserProfileManagement() {
+  return (
+    <section id="userProfile" className="w-screen h-screen" >
+      <div className="flex flex-col space-y-6">
+        <div className=""> USER PROFILE MANAGEMENT </div>
+
+        <div className="flex flex-col space-y-3"> 
+
+        </div>
+        
+      </div>
+    </section>
+  );
+}
