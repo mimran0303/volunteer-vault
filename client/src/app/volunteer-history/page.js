fd1e98069037fc1ce663aa492885618fc1a752c7
Tristan Fry <tristanfry1234@gmail.com>
2024-09-20 09:55:38 -0500

    finished notifcations page, touched up volunteer history page

--- client/src/app/volunteer-history/page.js
+++ client/src/app/volunteer-history/page.js
@@ -4,19 +4,19 @@ import React from 'react'
 
 import Link from 'next/link';
 
-import volunteer_history_bg from '../../public/sand.png'
+import volunteer_history_bg from '../../public/rectangle48.png'
 
 const VolunteerHistory = () => {
   return (
     <div
-      className="relative min-h-screen flex justify-center items-center bg-cover bg-center text-black"
+      className="bg-white bg-opacity-80 relative min-h-screen flex justify-center items-center bg-cover bg-center text-black"
       style={{
         backgroundImage: `url(${volunteer_history_bg.src})`,
       }}
     >
-      <div className="bg-white bg-opacity-90 p-12 rounded-lg shadow-md w-full md:w-[80%] lg:w-[100%] min-h-[750px]">
+      <div className=" p-12 rounded-lg  w-full md:w-[80%] lg:w-[100%] min-h-[750px]">
       {/* <div className="bg-white bg-opacity-90 p-8 rounded-lg shadow-md w-11/12 md:w-3/4 lg:w-2/3 overlfow-x-auto"> */}
-        <h1 className="text-3xl font-bold mb-6 text-center">VOLUNTEER HISTORY</h1>
+        <h1 className="text-3xl mb-6 text-center">VOLUNTEER HISTORY</h1>
         <table className="table-fixed w-full  min-w-full border-collapse border border-gray-400 text-left">
           <thead>
             <tr>
