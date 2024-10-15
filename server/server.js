c46648aa6887ad179b2c1a6c05305f627791df9a
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-10-14 19:14:29 -0500

    completed the volunteerHistory functionality

--- server/server.js
+++ server/server.js
@@ -35,12 +35,19 @@ const volunteerMatchRoute = require('./routes/volunteerMatchRoutes');
 const assignmentRoutes = require('./routes/assignmentRoutes');
 const notificationRoutes = require('./routes/notificationsRoutes');
 
+const volunteerHistoryRoutes = require("./routes/volunteerHistoryRoutes");
+
+
 app.use("/auth", authRoutes);
 app.use("/protected", protectedRoutes);
 
 app.use("/api/volunteers", volunteerMatchRoute);
 app.use("/api/assignments", assignmentRoutes);
 app.use('/api', notificationRoutes);
+
+app.use("/", volunteerHistoryRoutes);
+
+
 
 app._router.stack.forEach(function(r) {
   if (r.route && r.route.path) {
